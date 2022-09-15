78e21d28f300e3470e40e8561e51f71c5fb55bba
r-oleg-official <r.oleg.official@gmail.com>
2022-09-15 18:25:04 +0300

    Add seminar-2/task-003_shuffle_list.py.

--- seminar-2/task-003_shuffle_list.py
+++ seminar-2/task-003_shuffle_list.py
@@ -1,7 +1,42 @@
-# Реализуйте алгоритм перемешивания списка.
+
 
 # from random import randint
 from random import randint as rint
+
+# Idea 3.
+
+size_list = int(input('Enter size of a list: '))
+r_list = [0] * size_list
+for i in range(size_list):
+    r_list[i] = i
+print(r_list)
+
+exit()
+
+# Idea #2. Create list with random index. Go into all source list and replace only elements to this list of random index.
+
+
+def randomizier_shuff_index(number_index=1) -> int:
+    r_list = []
+    for i in range(number_index):
+        r_list.append(rint(0, number_index - 1))
+    return r_list
+
+
+size_list = int(input('Enter size of a list: '))
+s_list = randomizier_shuff_index(size_list)
+print(s_list)
+
+r_list_v2 = [size_list]
+for i in range(size_list):
+    print(i)
+    print(type(i))
+    r_list_v2[i] = i
+print(r_list_v2)
+r_list_v2 = set(r_list_v2)
+print(r_list_v2)
+
+exit()
 
 
 def randomizier_uniq_list(size_list) -> int:
@@ -31,5 +66,3 @@ def shuffle_list(source_list, number_circle):
 s_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 shuffle_list(s_list, 5)
 print(s_list)
-
-# Idea #2. Create list with random index. Go into all source list and replace only elements to this list of random index.
