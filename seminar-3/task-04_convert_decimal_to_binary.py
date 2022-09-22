fc708c74cdd3623504846557ba6faab64a954ded
r-oleg-official <r.oleg.official@gmail.com>
2022-09-22 17:12:49 +0300

    Add seminar-3/task-04_convert_decimal_to_binary.py.

---
+++ seminar-3/task-04_convert_decimal_to_binary.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+def dec_to_bin(num: int) -> str:
+    'Convert integer number to binary.'
+    if num == 0:
+        return 0
+    bin = ""
+    while (num > 0):
+        if (num % 2 == 0):
+            bin = "0" + bin
+        else:
+            bin = "1" + bin
+        num = num // 2
+    return bin
+
+
+number = int(input('Enter a number: '))
+print(dec_to_bin(number))
