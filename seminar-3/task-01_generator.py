d959ba82341329895042de542de81006d0981843
Oleg Razumov <r.oleg.official@gmail.com>
2022-09-29 10:36:15 +0300

    Edit seminar-3/task-01_generator.py/

--- seminar-3/task-01_generator.py
+++ seminar-3/task-01_generator.py
@@ -1,5 +1,29 @@
+# Реализуйте алгоритм задания случайных чисел без использования встроенного генератора 
+# псевдослучайных чисел.
+
 import time
 
 r = str(time.time()).split('.')
 print(r[1])
-# str(num).split('.')
+
+# Version 2. Nikolay.
+def my_random(minn, maxx):
+    time.sleep(0.3)
+    return int((time.time() % 1  * (maxx - minn)) + minn)
+#                     0.9                  7           1
+
+for i in range(10):
+    print(my_random(1, 9))
+
+# Ver.3.0.
+def random(a,b):
+    the_set = set()
+    list = []
+    for i in range(a,b):
+        the_set.add(str(i))
+    for e in the_set:
+        list.append(e)
+    print(list)
+
+
+random(5, 10)
