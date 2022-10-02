4818e4fb204dca9a19f5e62b1b3398dd021ab1e9
Oleg Razumov <r.oleg.official@gmail.com>
2022-10-02 17:52:10 +0300

    Add internet_requests.

---
+++ internet_requests/requests_url.py
@@ -0,0 +1,12 @@
+# https://youtu.be/H94Aicz-Hyc
+import requests # request to Internet and get data from Internet.
+# import lxml
+
+
+def main():
+    return requests.get(url='https://google.com')
+
+
+if __name__ == '__main__':
+    print(main())
+    
