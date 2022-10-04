7277fd7004d41ece67cadde057e204a64131421f
r-oleg-official <r.oleg.official@gmail.com>
2022-10-04 17:06:50 +0300

    Add seminar-4/task-01_calc_pi_accuracy_d.py.

---
+++ seminar-4/task-01_calc_pi_accuracy_d.py
@@ -0,0 +1,17 @@
+
+
+
+from cmath import sin
+from math import sqrt
+from numpy import Arcsin
+x = 9876
+pi = 2 * (Arcsin(sqrt (1 - x ^ 2))) + abs(Arcsin (х))
+
+pi = 
+
+exit()
+
+# d = int(input('Enter accuracy d: '))
+x = 9876
+pi = x * sin(180/x)
+print(pi)
