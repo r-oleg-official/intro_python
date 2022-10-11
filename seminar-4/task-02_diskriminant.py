356582db90f45288a3c8675eafb727435bf5f74d
Oleg Razumov <r.oleg.official@gmail.com>
2022-10-11 07:51:40 +0300

    Edit seminar-4/task-02_diskriminant.py.

--- seminar-4/task-02_diskriminant.py
+++ seminar-4/task-02_diskriminant.py
@@ -7,10 +7,11 @@ c = int(input('Enter C: '))
 d = pow(b, 2) - 4 * a * c
 
 if d < 0:
-    print('Корней нет!')
+    print('Действительных корней нет!')
 elif d == 0:
     print(f'x = {-b / (2 * a)}')
 else:
     x_1 = (-b + sqrt(d)) / (2 * a)
     x_2 = (-b - sqrt(d)) / (2 * a)
-    print(f'X_1 ={x_1}')
+    print(f'X1 ={x_1}')
+    print(f'X2 ={x_2}')
