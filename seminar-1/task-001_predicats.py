a058cf8da1908a66e5813825bb0fafc000b64dd0
r-oleg-official <r.oleg.official@gmail.com>
2022-11-25 16:53:02 +0300

    Edit seminar-1/task-001_predicats.py.

--- seminar-1/task-001_predicats.py
+++ seminar-1/task-001_predicats.py
@@ -5,17 +5,24 @@
 # print('\u2228 - or, \u2227 - and, \u00AC - not sign')
 
 
-# def switch(sw):
-#     if sw == 0:
-#         return 1
-#     else:
-#         return 0
+def checkPredicates(startNumber: int, endNumber: int):
+    for i in range(startNumber, endNumber + 1):
+        x = i
+        for j in range(startNumber, endNumber + 1):
+            y = j
+            for k in range(startNumber, endNumber + 1):
+                z = k
+                if not(x or y or z) == (not (x) and not (y) and not (z)):
+                    # print(f'X= {x}, Y= {y}, Z= {z}')
+                    print(f'{x}\t {y}\t {z}\t')
 
-for i in range(2):
-    x = i
-    for j in range(2):
-        y = j
-        for k in range(2):
-            z = k
-            if not (x or y or z) == (not x) and (not y) and (not z):
-                print(f'X={x}, Y={y}, Z={z}')
+
+def main():
+    startNumber = int(input('Введите начальное значение для X, Y, Z: '))
+    endNumber = int(input('Введите конечное значение для X, Y, Z: '))
+    print(f"X \tY \tZ")
+    checkPredicates(startNumber, endNumber)
+
+
+if __name__ == "__main__":
+    main()
