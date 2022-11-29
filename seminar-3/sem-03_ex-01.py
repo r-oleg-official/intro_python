2addf18f72f87a66fffdf4879d15e78532c84666
Oleg Razumov <r.oleg.official@gmail.com>
2022-11-29 18:41:50 +0300

    Edit seminar-3/sem-03_ex-01.py.

--- seminar-3/sem-03_ex-01.py
+++ seminar-3/sem-03_ex-01.py
@@ -1,19 +1,25 @@
 # Реализуйте алгоритм задания случайных чисел без использования встроенного генератора 
 # псевдослучайных чисел.
 
+# Ver.1.0
 import time
 
 r = str(time.time()).split('.')
 print(r[1])
 
-# Version 2. Nikolay.
+
+# Ver.2.0 Nikolay.
+# minn - maxx - range random numbers
 def my_random(minn, maxx):
-    time.sleep(0.3)
+    time.sleep(0.2)
     return int((time.time() % 1  * (maxx - minn)) + minn)
 #                     0.9                  7           1
 
+
 for i in range(10):
-    print(my_random(1, 9))
+    # 10 - amount of digits
+    print(my_random(1, 9), end=' ')
+
 
 # Ver.3.0.
 def random(a,b):
@@ -27,3 +33,28 @@ def random(a,b):
 
 
 random(5, 10)
+
+# Ver.4.0 
+from datetime import datetime
+
+
+def get_random_number(n):
+    now = datetime.now()
+    random_number = now.time().second ** now.time().minute * now.time().microsecond
+    return random_number % 10**n
+
+
+print(get_random_number(3))
+
+# Ver.5.0.
+import time
+
+
+def func (n):
+    x = time.time() * (10 ** n)
+    x %= 10 ** n
+    return(round(x))
+
+
+print(func(3))
+
