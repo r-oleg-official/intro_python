501b7468ca0a79b9991f6d5e0be073dac0432877
Oleg Razumov <r.oleg.official@gmail.com>
2022-11-30 15:42:28 +0300

    Done seminar-3/task-01_sum_uneven_list.py.

--- seminar-3/task-01_sum_uneven_list.py
+++ seminar-3/task-01_sum_uneven_list.py
@@ -1,21 +1,30 @@
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 # Пример:
-# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+# [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-from funcs import gen_rand_list as r_list
+from funcs import gen_rand_list
 
-s_list = [2, 3, 5, 9, 3]
 
-# Universal list:
-# size_list = int(input('Enter a size of list: '))
-# start_list = int(input('Enter a first number of list: '))
-# stop_list = int(input('Enter a last number of list: '))
-# s_list = r_list(start_list, stop_list, size_list)
+def choice_list() -> list:
+    user_choice = int(input('Choice: 1 - task, 2 - user list --> '))
+    match user_choice:
+        case 1:
+            s_list = [2, 3, 5, 9, 3]
+        case 2:
+            # Create a universal list:
+            size_list = int(input('Enter a size of list: '))
+            start_list = int(input('Enter a first number of list: '))
+            stop_list = int(input('Enter a last number of list: '))
+            s_list = gen_rand_list(start_list, stop_list, size_list)
+    return s_list
+
+
+user_list =choice_list()
+uneven_elements = []
 
 sum = 0
-pos_list = []
-for i in range(1, len(s_list), 2):
-    sum += s_list[i]
-    pos_list.append(s_list[i])
+for i in range(1, len(user_list), 2):
+    sum += user_list[i]
+    uneven_elements.append(user_list[i])
 print(
-    f'- {s_list} -> на нечётных позициях элементы {pos_list}. Ответ: {sum}')
+    f'{user_list} -> на нечётных позициях элементы {uneven_elements}. Ответ: {sum}')
