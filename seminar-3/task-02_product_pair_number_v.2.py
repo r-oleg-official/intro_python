b0c3d0992db53c82a1fcd7448688242214c3c7fd
Oleg Razumov <r.oleg.official@gmail.com>
2022-11-30 15:59:29 +0300

    Done seminar-3/task-02_product_pair_number.py.

--- seminar-3/task-02_product_pair_number_v.2.py
+++ seminar-3/task-02_product_pair_number_v.2.py
@@ -7,13 +7,11 @@ from funcs import gen_rand_list
 
 
 def choice_list() -> list:
-    user_choice = int(input('Choice: 1, 2 - task, 3 - user list --> '))
+    user_choice = int(input('Choice: 1 - task, 2 - user list --> '))
     match user_choice:
         case 1:
             s_list = [2, 3, 4, 5, 6]
         case 2:
-            s_list = [2, 3, 5, 6]
-        case 3:
             # Create a universal list:
             size_list = int(input('Enter a size of list: '))
             start_list = int(input('Enter a first number of list: '))
@@ -24,11 +22,27 @@ def choice_list() -> list:
 
 user_list = choice_list()
 middle = len(user_list) // 2
-reverse_list = user_list[::-1]
 result_list = []
 
-for i in range(middle):
-    result_list.append(user_list[i] * reverse_list[i])
+res_list = []
+i, j = 0, (len(user_list) - 1)
+
+while i < middle:
+    res_list.append(user_list[i] * user_list[j])
+    i += 1
+    j -= 1
 if (len(user_list) % 2 != 0):
-    result_list.append(user_list[middle])
-print(f'- {user_list} => {result_list}')
+    res_list.append(user_list[middle])
+print(f'- {user_list} => {res_list}')
+
+exit()
+
+# Ver.3.0
+middle = len(s_list) // 2
+tmp_list = deque(s_list[:])
+res_list = []
+for i in range(middle):
+    res_list.append(tmp_list.popleft() * tmp_list.pop())
+if (len(s_list) % 2 != 0):
+    res_list.append(s_list[middle])
+print(f'- {s_list} => {res_list}')
