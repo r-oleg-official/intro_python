48f43e874590f16b12d26ccaa1ccf3e27da7ce0c
Oleg Razumov <r.oleg.official@gmail.com>
2022-12-05 18:41:56 +0300

    Edit seminar-4/task-05_summa_polinomes.py seminar-4/polinom-1.txt.

--- seminar-4/task-05_summa_polinomes.py
+++ seminar-4/task-05_summa_polinomes.py
@@ -1,33 +1,54 @@
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-from random import randint
+def read_file(path: str):
+    with open(path, 'r') as file:
+        data = file.readline()
+    return data
+
+def parse_str(str: str) -> list:
+    li = []
+    start_pos = 0
+    i = 0
+    while i < len(str):
+        if (str[i] == '+') or (str[i] == '-') or (str[i] == '='):
+                
+            li.append(str[start_pos:i])
+            start_pos = i
+        i +=1
+    li[len(li) -1] += 'x^0'
+
+    members = []
+    for item in li:
+        members.append(tuple(map(int, item.split('x^'))))
+    return members
 
 
-def gen_rand_list(start, stop, size) -> int:
-    random_list = [0] * size
-    for i in range(size):
-        random_list[i] = randint(start, stop + 1)
-    return random_list
-
-
-def create_polinom(factor_list: int, degree: int, result: str) -> str:
-    if degree == 0: return result
-    if factor_list[degree] != 0:
-        result = result + '+' + str(factor_list[degree]) + 'x^' + str(degree)
-    return create_polinom(factor_list, degree - 1, result)
-
-
-def save_to_txt(file: str, str_line: str):
-    with open(file, 'w') as data:
-        data.write(str_line)
-
-
-
+def format_polinom(li: list) -> list:
+    for i in range(len(li) - 1):
+        sub_str = li[i] + li[i + 1]
+        if sub_str == '**':
+            li = li[:i] + '^' + li[i + 2:]
+        if sub_str == 'x+' or sub_str == 'x-':
+            li = li[:i] + 'x^1' + li[i + 1:]
+    return li
 
 
 def main():
-    x = 1
+    path_polinom_1 = 'polinom-1.txt'
+    path_polinom_2 = 'polinom-2.txt'
+    polinom_1 = format_polinom(read_file(path_polinom_1))
+    polinom_2 = format_polinom(read_file(path_polinom_2))
+    print(polinom_1)
+    print(polinom_2)
+    print()
+    members_polinom_1 = parse_str(polinom_1)
+    print(members_polinom_1)
+    print()
+    members_polinom_2 = parse_str(polinom_2)
+    print(members_polinom_2)
+    print(members_polinom_1[4][1])
+
 
 if __name__ == "__main__":
     main()
