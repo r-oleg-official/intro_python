3a5338ce9534b3cb2f6efa934a2a3e53c9bb36ae
r-oleg-official <r.oleg.official@gmail.com>
2022-12-07 21:12:38 +0300

    Edit task-05_summa_polinomes.py, add summ_polinomes.txt.

--- seminar-4/task-05_summa_polinomes.py
+++ seminar-4/task-05_summa_polinomes.py
@@ -1,10 +1,32 @@
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
+
 def read_file(path:str):
     with open(path,'r') as file:
         data = file.readline()
     return data
+
+
+def write_to_file(path: str, li: list):
+    data = f'{li[0][1]}x^{li[0][0]}'
+    
+    for i in range(1, len(li) - 1):
+        if li[i][1] > 0:
+            data += f'+{li[i][1]}x^{li[i][0]}'
+        if li[i][1] < 0:
+            data += f'{li[i][1]}x^{li[i][0]}'
+    
+    if li[-2][1] > 0:
+        data += f'+{li[-2][1]}x'
+    else: data += f'{li[-2][1]}x'
+    if li[-1][1] > 0:
+        data += f'+{li[-1][1]}=0'
+    else: data += f'{li[-1][1]}=0'
+    print(data)
+
+    with open(path,'w') as file:
+        file.write(data)
 
 
 def format_polinom(li: list) -> list:
@@ -35,7 +57,6 @@ def parse_str(str: str)->list:
         if len(tmp) == 1:
             tmp.append(0)
         members.append(tuple(tmp))
-
     return members
 
 
@@ -47,106 +68,52 @@ def find_max_degree(li_1: list, li_2: list) -> int:
     
     for i in range(len(li_2)):
         if li_2[i][1] > max_degree: max_degree = li_2[i][1]
-    
     return max_degree
+
+
+def add_lost_element(li: list, degree: int) -> list:
+    full_indexes = [x for x in range(degree + 1)][::-1]
+    li_indexes = []
+
+    for item in li:
+        li_indexes.append(item[1])
+    
+    for item in full_indexes:
+        if item not in li_indexes: li.append(tuple([0, item]))
+    return li
 
 
 def sum_polinomes(polinom_1: list, polinom_2: list) -> list:
     res_polinom = []
     max_degree = find_max_degree(polinom_1, polinom_2)
+    polinom_1 = add_lost_element(polinom_1, max_degree)
+    polinom_2 = add_lost_element(polinom_2, max_degree)
+
+    for i in polinom_1:
+        for j in polinom_2:
+            if i[1] == j[1]:
+                res_polinom.append(tuple([i[1], i[0] + j[0]]))
     
+    for item in res_polinom:
+        if item[0] == 0: res_polinom.remove(item)
 
-    for i in range(max_degree + 1):
-        count, index = 0, 0
-        for item in polinom_1:
-            if i != item[1]: res_polinom.append(item)
-            # print(item[1])
-        # print(f'count ={count}')
-        #if count == 0: res_polinom.append(tuple(polinom_1[i]))
-
-        # for item in polinom_2:
-            # if i == item[1]: count_2 += 1
-        # if count_1 == 0: polinom_1 = polinom_1.append(tuple(polinom_1[i]))
-        # if count_2 == 0: polinom_1 = polinom_1.append(tuple(polinom_1[i]))
-        # if (count_1 == count_2 > 0): 
-            # res_polinom.append(polinom_1[i][0] + polinom_2[i][0])
-        
-
-    # for i in range(max_degree + 1):
-        # count = 0
-        # for item in polinom_1:
-            # if i == item[1]: count += 1
-        # if count > 0: res_polinom.append(polinom_1[i])
-        # if count == 0: polinom_1 = polinom_1.append(tuple([0, i]))
-
-    # for i in range(max_degree + 1):
-        # count = 0
-        # for item in polinom_2:
-            # if i == item[1]: count += 1
-        # if count > 0: res_polinom.append(polinom_2[i])
-        # if count == 0: polinom_2.append(tuple([0, i]))
-
-
-
-
-
-    # for i in range(len(polinom_1)):
-        # if polinom_1[i][1] > max_degree:
-            # max_degree = polinom_2[i][1]
-    # print(f'max_degree {max_degree}')
-
-    # for i in range(find_max_degree(polinom_1) + 1):
-        # for j in range(len(polinom_1)):
-            # if polinom_1[]
-# 
-    # print(find_max_degree(polinom_2))
-    # print(max_pol[0][1])
-    # print(type(min_pol[0][0]))
-
-
-    # for i in range(max_degree +1):
-        # try:
-            # tmp = max_pol[i][0] + min_pol[i][0]
-        # except ValueError:
-            # res_polinom.app
-
-    # Version 1
-    # for i in range(max_degree +1):
-        # if max_pol[i][1] == i:
-            # res_polinom.append(max_pol[i][0] + min_pol[i][0])
-        # if min_pol[i][1] == i:
-            # res_polinom.append(max_pol[i][0])
-    
-
-
-    # Version 2.
-    # for i in range(max_len):
-        # for j in range(min_len):
-            # if max_pol[i][1] == min_pol[j][1]:
-                # print(max_pol[0][j] + min_pol[0][j])
-                # res_polinom.append(max_pol[0][j] + min_pol[0][j])
-    
+    res_polinom.sort()
+    res_polinom.reverse()
     return res_polinom
-
 
 
 def main():
     path_polinom_1 = 'polinom-1.txt'
+    # -81x^4+30x**3-22x^2+97x+14=0 
     path_polinom_2 = 'polinom-2.txt'
+    # 62x^5+97x^4+35x^3+69x^2+94x^1+62=0 
+    path_sum_polinomes = 'summ_polinomes.txt'
     polinom_1 = format_polinom(read_file(path_polinom_1))
     polinom_2 = format_polinom(read_file(path_polinom_2))
-    # print(polinom_1)
-    # print(polinom_2)
-    # print()
     members_polinom_1 = parse_str(polinom_1)
-    print(members_polinom_1)
     members_polinom_2 = parse_str(polinom_2)
-    print(members_polinom_2)
-    print()
-    print(sum_polinomes(members_polinom_1, members_polinom_2))
-    # print(members_polinom_2)
-    # print(members_polinom_1[4][0])
-
+    write_to_file(path_sum_polinomes, sum_polinomes(members_polinom_1, members_polinom_2))
+    
 
 if __name__ == "__main__":
     main()
