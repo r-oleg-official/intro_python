2d8145380586a8e965f3c394b031945cb795dd9c
r-oleg-official <r.oleg.official@gmail.com>
2022-12-16 10:14:57 +0300

    Edit seminar-05/task-03_tic-tac-toe_console. In progress.

--- seminar-05/task-03_tic-tac-toe_console/db.py
+++ seminar-05/task-03_tic-tac-toe_console/db.py
@@ -15,3 +15,18 @@ def logger():
 
 mode = ['1 - Player vs Player', '2 - Player vs PC', '3 - PC vs PC', '4 - Exit game']
 new_board = ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ']
+
+mode = ['| 2 - Player vs PC     |', '| 4 - Exit game        |']
+mode2 = ['| 1 - Player vs Player |', '| 2 - Player vs PC     |', '| 3 - PC vs PC         |', '| 4 - Exit game        |']
+
+# Winner's combo
+#            0      1      2      3      4      5      6      7      8      9
+board_1 = ['   ', ' X ', '   ', '   ', '   ', ' X ', '   ', '   ', '   ', ' X ']
+board_2 = ['   ', '   ', '   ', ' X ', '   ', ' X ', '   ', ' X ', '   ', '   ']
+board_3 = ['   ', ' X ', ' X ', ' X ', '   ', '   ', '   ', '   ', '   ', '   ']
+board_4 = ['   ', '   ', '   ', '   ', ' X ', ' X ', ' X ', '   ', '   ', '   ']
+board_5 = ['   ', '   ', '   ', '   ', '   ', '   ', '   ', ' X ', ' X ', ' X ']
+board_6 = ['   ', ' X ', '   ', '   ', ' X ', '   ', '   ', ' X ', '   ', '   ']
+board_7 = ['   ', '   ', ' X ', '   ', '   ', ' X ', '   ', '   ', ' X ', '   ']
+board_8 = ['   ', '   ', '   ', ' X ', '   ', '   ', ' X ', '   ', '   ', ' X ']
+
