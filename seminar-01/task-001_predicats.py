7218c6babb6b67d1e700bfdd205386693f3b78a5
r-oleg-official <r.oleg.official@gmail.com>
2022-12-22 12:24:33 +0300

    Edit seminar-01/task-001_predicats.py.

--- seminar-01/task-001_predicats.py
+++ seminar-01/task-001_predicats.py
@@ -11,7 +11,7 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             left = not(x or y or z)
-            right = (not(x)) and (not(y)) and (not(z))
+            right = (not x) and (not y) and (not z)
             if left == right: 
                 print(f'{x}\t {y}\t {z}\t')
 
@@ -23,6 +23,6 @@ for x in [True, False]:
     for y in [True, False]:
         for z in [True, False]:
             left = not(x or y or z)
-            right = (not(x)) and (not(y)) and (not(z))
+            right = (not x) and (not y) and (not z)
             if left == right: 
                 print(f'{x}\t {y}\t {z}\t')
