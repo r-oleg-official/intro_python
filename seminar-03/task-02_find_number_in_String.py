660825d6c2db6ad37ee85023e0c49c89dc31366d
r-oleg-official <r.oleg.official@gmail.com>
2022-12-27 15:26:37 +0300

    Edit seminar-03/task-02_find_number_in_String.py.

--- seminar-03/task-02_find_number_in_String.py
+++ seminar-03/task-02_find_number_in_String.py
@@ -1,21 +1,18 @@
-# 2. Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
-# ['efg23', '79234gefg', 'sdgs', 'g53']
-# Поиск'2'
-# ['efg23', '79234gefg']
+"""# 2. Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
+['efg23', '79234gefg', 'sdgs', 'g53']
+Поиск'2'
+['efg23', '79234gefg'] """
 
 
-def search_str (base_string, find_str):
-    list_res = []
-    for i in base_string:
-        if find_str in i:
-            list_res.append(i)
-    return list_res
+def sort_by_str(li: list, sub_string: str) -> list:
+    res_li = []
+    for i in li:
+        if sub_string in i:
+            res_li.append(i)
+    return res_li
 
 
 list_1 = ['efg23', '79234gefg', 'sdgs', 'g53']
-find_str = '2'
-
-list_res = search_str(list_1, find_str)
-
-print(list_res)
-
+find_string = '2'
+res_list = sort_by_str(list_1, find_string)
+print(res_list)
